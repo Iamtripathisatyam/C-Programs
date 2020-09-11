2920aff1a352ffd4d7eb07a9dccb33d2901fd0ac
Satyam Tripathi <69134468+Iamtripathisatyam@users.noreply.github.com>
2020-09-11 14:52:04 +0530

    Linear_Searching

--- Linear_Searching.c
+++ Linear_Searching.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-main(satyam)
+int main(satyam)
 {
 	int n,key,flag=0,arr[20],i,pos;
 	printf("\n Enter The No. of Elements:");
@@ -9,7 +9,7 @@ main(satyam)
 	{
 		scanf("%d",&arr[i]); //Inputing of numbers.....
 	}
-		printf("\n Enter The No. That You Wants You Search...\n");
+		printf("\n Enter The No. That You Wants You Search : \n");
 		scanf("%d",&key); // Inputing of key...
 
 	for(i=0;i<n;i++) //// Linear Searching......
@@ -22,7 +22,7 @@ main(satyam)
 		}
 }
 if(flag==0) ///Means key is not found....
-		printf("\n %d is not found In given Nos.....",key);
+		printf("\n %d is not found In given Nos!",key);
 	
 }
 
