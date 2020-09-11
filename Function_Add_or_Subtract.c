9eda544f849cc2a55b1e366a8e263e2cef166c40
Satyam Tripathi <69134468+Iamtripathisatyam@users.noreply.github.com>
2020-09-11 14:01:13 +0530

    Add files via upload

---
+++ Function_Add_or_Subtract.c
@@ -0,0 +1,21 @@
+float ADD(float a,float b)
+{
+	float s;
+	s=a+b;
+	return a+b;
+}
+float SUB(float a,float b)
+{
+	float s;
+	s=a-b;
+	return s;
+}
+main(satyamtripathi)
+{
+	float a,b;
+	printf("\n Please Enter Two No:");
+	scanf("%f%f",&a,&b);
+	printf("\n Sum of No. Are:%f",ADD(a,b));
+	printf("\n Diff. of No. Are:%f",SUB(a,b));	
+	return 0;
+}
