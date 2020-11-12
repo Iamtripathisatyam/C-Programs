ea560d485093c715e3c8ef7da1569c1abfeec1ff
Satyam Tripathi <69134468+Iamtripathisatyam@users.noreply.github.com>
2020-11-13 00:09:52 +0530

    Add files via upload

---
+++ C Programs/UpperCase-To_LowerCase.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(satyamtripathi)
+{
+	char uc,lc;
+	int y=lc;
+	printf("Enter The Upper case Letter:");
+	scanf("%c",&uc);
+	lc=uc+32;
+	printf("\nThe Character in Lower Case:%c",lc);
+	printf("\nThe ASCII value is:%d",lc);
+}
